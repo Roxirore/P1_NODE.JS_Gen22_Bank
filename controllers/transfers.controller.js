1713e46affd213d0f10f2b745f9b0dfe10398779
roxirore <roxana.rod.rev@gmail.com>
2023-05-05 05:08:21 -0500

    transfers

--- controllers/transfers.controller.js
+++ controllers/transfers.controller.js
@@ -1,105 +1,101 @@
 const catchAsync = require('../utils/catchAsync');
 const Transfer = require('./../models/transfers.model');
-// //controllers sin id
-
+const { updatedPassword } = require('./auth.controller');
 
 exports.createTransfer = catchAsync (async (req, res) => {
 
-
-    const { senderUserId, receiverUserId, monto } =
-        req.body;
+    // 1. recibir el amount, accountNumber, senderUserId de la req.body
+    const { amount, accountNumber, senderUserId } = req.body;
   
-    const transfer = await Transfer.create({
-        senderUserId,
-        receiverUserId,
-    });
-  
-    await transfer.update({ amount: (amount ) });
+    // 2. buscar el usuario (userReceiver) que va a  recibir el monto con findOne, con status: true y 
+    // accountNumber: accountNumber 
+    const userReceiver = await User.findOne({
+        where: {
+            accountNumber,
+            status: 'available',
+        },
+      });
 
+    // 3. crear una constante que se llame receiverUserId = UserReceiver.id
+    const receiverUserId = userReceiver.id;
+
+    // 4. buscar al usuario que hace la transferencia (userTransfer) con el senderUserId
+    const userTransfer = await User.findOne({
+        where: {
+            status: 'available',
+            id: senderUserId,
+        }
+    })
+
+    // 5. verificar si el monto a transferir es mayor al monto que tiene userTransfer, 
+    // enviar error 400
+    if (amount > userTransfer.amount) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'The amount in the account is less than the amount you want to transfer',
+          });
+    }
+
+    // 6. verificar si el UserTransfer.id = UserReceiver.id, enviar error
+    if (userTransfer.id === userReceiver.id) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'The user cannot send money to his own account',
+          });
+    }
+
+    // 7. crear una constante que se llame newAmountUserTransfer, en ella va el resultado
+    // de restar el UserTransfer.amount menos el amount de la req.body
+    const newAmountUserTransfer = userTransfer.amount - amount;
+
+    // 8. crear una constante que se llame newAmountUserReceiver, en ella va el resultado
+    // de sumar el userReceiver.amount mas el amount de la req.body
+    const newAmountUserReceiver = userReceiver.amount + amount;
+
+    // 9. actualizar la informacion del userTransfer con su nuevo amount
+    await userTransfer.update({amount: newAmountUserTransfer})
+
+    // 10. actualizar la informacion del userReceiver con su nuevo amount
+    await userReceiver.update({amount: newAmountUserReceiver})
+
+    // 11. guardar o crear la transferencia en la base de datos
+    await Transfer.create({ amount, senderUserId, receiverUserId})
+
+    // 12. enviar la respuesta al cliente que la transferencia fue exitosa
     res.status(201).json({
       status: 'success',
       message: 'The transfer has been done!',
-      repair,
+      Transfer,
     });
 
 });
 
 
-// //controllers con id
 
-exports.updateRepair = catchAsync (async (req, res) => {
-
-
-    // traer el userid de la res.params
-    const { id } = req.params;
-    // buscar la repair a actualizar
-    const repair = await Repair.findOne({
-      where: {
-          id,
-          status: 'pending',
-      },
-    });
-    // validar si la repair existe, sino error
-    if (!repair) {
-      return res.status(404).json({
-        status: 'error',
-        message: `the repair with id: ${id} is not found`,
-      });
-    }
-    // usar el update para pasar el estado a pending o completed
-    await repair.update({ status: 'completed' });
-    res.status(200).json({
-      status: 'success',
-      message: 'the repair has been completed',
-    });
-
-
-});
-
-exports.deleteRepair = catchAsync (async (req, res) => {
-    
-    // traer el userid de la res.params
-    const { id } = req.params;
-    // buscar la repair a actualizar
-    const repair = await Repair.findOne({
-      where: {
-          id,
-          status: 'pending' 
-      },
-    });
-    // validar si la repair existe, sino error
-    if (!repair) {
-      return res.status(404).json({
-        status: 'error',
-        message: `the repair with id: ${id} is not found`,
-      });
-    }
-    // usar el update para pasar el estado a pending o completed
-    await repair.update({ status: 'cancelled' });
-    res.status(200).json({
-      status: 'success',
-      message: 'the repair has been cancelled',
-    });
-
-});
-
-exports.findOneRepair = catchAsync (async (req, res) => {
-    
-    const { id } = req.params;
-  
-    const repair = await Repair.findOne({
-      where: {
-        id,
-      },
-    });
-    if (!repair) {
-      return res.status(404).json({
-        message: `The repair with id ${id} not found`,
-      });
-    }
-    res.status(200).json({
-      message: 'The repair has been found successfully',
-      repair,
-    });
-
-});
+// PASOS
+// 1. reciibir el amount, accountNumber, senderUserId de la req.body
+// 2. buscar el usuario (userReceiver) que va a  recibir el monto con findOne, con status: true y 
+// accountNumber: accountNumber 
+// 3. crear una constante que se llame receiverUserId = UserReceiver.id
+// 4. buscar al usuario que hace la transferencia (userTransfer) con el senderUserId
+    // const userTransfer = await User.findOne({
+    //     where: {
+    //         status: true,
+    //         id: senderUserId,
+    //     }
+    // })
+// 5. verificar si el monto a transferir es mayor al monto que tiene userTransfer, 
+// enviar error 400
+    // if (amount > userTransfer.amount) {mensaje de error}
+// 6. verificar si el UserTransfer.id = UserReceiver.id, enviar error
+// 7. crear una constante que se llame newAmountUserTransfer, en ella va el resultado
+// de restar el UserTransfer.amount menos el amount de la req.body
+// 8. crear una constante que se llame newAmountUserReceiver, en ella va el resultado
+// de sumar el userReceiver.amount mas el amount de la req.body
+// 9. actualizar la informacion del userTransfer con su nuevo amount
+    // await userTransfer.update({amount: newAmountUserTransfer})
+// 10. actualizar la informacion del userReceiver con su nuevo amount
+    // await userReseirver.update({amount: newAmountUserReceiver})
+// 11. guardar o crear la transferencia en la base de datos
+    // await Transfer.create({ amount, senderUserId, receiverUserId})
+// 12. enviar la respuesta al cliente que la transferencia fue exitosa
