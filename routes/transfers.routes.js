0e3cf8dccf80c11b648157962649dd13291566e4
roxirore <roxana.rod.rev@gmail.com>
2023-05-05 05:34:53 -0500

    utils

--- routes/transfers.routes.js
+++ routes/transfers.routes.js
@@ -1,1 +1,17 @@
-const express = require('express');
+const express = require('express')
+const transfersController = require('./../controllers/transfers.controller')
+
+const transfersRouter = express.Router();
+
+transfersRouter
+.route('/')
+.get(transfersController.findAllTransfersPending)
+.post(transfersController.createTransfer)
+
+transfersRouter
+.route('/:id')
+.get(transfersController.findOneTransfer)
+.patch(transfersController.updateTransfer)
+.delete(transfersController.deleteTransfer)
+
+module.exports = repairsRouter;
